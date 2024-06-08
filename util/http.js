bcb079bcc76a3f986fc29a3016085048ad37d977
nuttyartist <awesomeness.notes@gmail.com>
2024-06-08 12:53:11 +0300

    Update packages

--- util/http.js
+++ util/http.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 const BACKEND_URL =
-  "http://rhymeit-env.eba-ks9v23db.us-west-2.elasticbeanstalk.com";
+  "http://rhymeit-env.eba-5i5mmee7.us-west-2.elasticbeanstalk.com";
 
 export async function fetchRyhmes(input1, input2) {
+  console.log("Here 0");
+
   const response = await axios.get(BACKEND_URL, {
     params: { firstInputs: input1, secondInputs: input2 },
   });
 
-  // console.log(response);
+  console.log("Here");
+
+  console.log(response);
 
   // const rhymeData = [];
 
